f6639a3920e954264adc81fa490848474c89ad05
Henrique <henriquels.dev@gmail.com>
2025-07-04 01:34:19 -0300

    fix: Correct base path in vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: 'https://github.com/Henriquelss/consulta-veicular-fipe',
+  base: '/consulta-veicular-fipe/',
   plugins: [
     tailwindcss(),
   ],
